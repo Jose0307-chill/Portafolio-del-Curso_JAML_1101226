6d9dfc8ec3d73280fac18724ea7f5587e0b6f460
Jose0307-chill <JAMENDEZLO@correo.url.edu.gt>
2026-05-20 22:26:54 -0600

    Laboratorio #3
    Se añadió manualmente, laboratorio #3

---
+++ L3_JAML_1101226/Laboratorio/Program.cs
@@ -0,0 +1,9 @@
+﻿// Este es un comentario - será ignorado en tiempo de ejecución
+Console.WriteLine("José Méndez - 1101226");
+Console.WriteLine("Laboratorio # 03");
+// Ingresar aquí nombre y carnet del dueño del repositorio 
+José Méndez - 1101226
+// Ingresar aquí nombre y carnet del colaborador:
+Erick de León - 1192926
+Console.WriteLine("Erick De Leon - 1192926");
+Console.ReadLine();
---
+++ L3_JAML_1101226/README.md
@@ -0,0 +1,6 @@
+# Laboratorio #03
+
+| Nombre | Carnet |
+|------------------|-----------|
+| José Méndez | 1101226 |
+| Erick De Leon | 1192926 |
