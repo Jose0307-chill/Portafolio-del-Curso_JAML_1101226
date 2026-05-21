a7edebd0b3d242d4bb374b1a7449f3d851e5daf9
Jose0307-chill <JAMENDEZLO@correo.url.edu.gt>
2026-05-20 22:28:39 -0600

    Laboratorio #5
    Se añadió laboratorio #5

---
+++ L5_JAML_1101226/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Desafío #1: Identificación de Número");
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Ingrese un número:");
+        int num = int.Parse(Console.ReadLine()); //Entrada
+        if (num == 0)
+        {
+            Console.WriteLine("El número ingresado es neutro, osea: " + num); //Salida
+        }
+        else if (num > 0)
+        {
+            Console.WriteLine("El número ingresado es positivo"); //Salida
+        }
+        else
+        {
+            Console.WriteLine("El número ingresado es negativo"); //Salida
+        }
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Desafío #2: Año bisiesto");
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Ingrese un año:");
+        int annio = int.Parse(Console.ReadLine()); //Entrada
+        if (annio <= 0)
+        {
+            Console.WriteLine("El año no puede ser 0 o un número negativo"); //Salida
+        }
+        else if (annio % 4 == 0 && annio % 100 != 0 || annio % 400 == 0)
+        {
+            Console.WriteLine("El año ingresado es bisiesto"); //Salida
+        }
+        else
+        {
+            Console.WriteLine("El año ingresado no es bisiesto"); //Salida
+        }
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Desafío #3: Boleto de ornato");
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Ingrese su salario mensual:");
+        float salario = float.Parse(Console.ReadLine()); //Entrada
+        Console.WriteLine("¿Usted tiene multa? Escriba el número correspondiente (Si = 1, No = 0)");
+        bool multa = Console.ReadLine() == "1"; //Entrada
+        if (salario <= 500)
+        {
+            Console.WriteLine("Usted no debe pagar ornato"); //Salida
+        }
+        else if (salario > 500 && salario <= 1000)
+        {
+            if (!multa)
+                Console.WriteLine("Usted debe pagar: Q10.00"); //Salida
+            else
+            {
+                Console.WriteLine("Usted debe pagar: Q20.00"); //Salida
+            }
+        }
+        else if (salario > 1000 && salario <= 3000)
+        {
+            if (!multa)
+                Console.WriteLine("Usted debe pagar: Q15.00"); //Salida
+            else
+            {
+                Console.WriteLine("Usted debe pagar: Q30.00"); //Salida
+            }
+        }
+        else if (salario > 3000 && salario <= 6000)
+        {
+            if (!multa)
+                Console.WriteLine("Usted debe pagar: Q50.00"); //Salida
+            else
+            {
+                Console.WriteLine("Usted debe pagar: Q100.00"); //Salida
+            }
+        }
+        else if (salario > 6000 && salario <= 9000)
+        {
+            if (!multa)
+                Console.WriteLine("Usted debe pagar: Q75.00"); //Salida
+            else
+            {
+                Console.WriteLine("Usted debe pagar: Q150.00"); //Salida
+            }
+        }
+        else if (salario > 9000 && salario <= 12000)
+        {
+            if (!multa)
+                Console.WriteLine("Usted debe pagar: Q100.00"); //Salida
+            else
+            {
+                Console.WriteLine("Usted debe pagar: Q200.00"); //Salida
+            }
+        }
+        else
+        {
+            if (!multa)
+                Console.WriteLine("Usted debe pagar: Q150.00"); //Salida
+            else
+            {
+                Console.WriteLine("Usted debe pagar: Q300.00"); //Salida
+            }
+        }
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Desafío #4: Sistema de parqueo");
+        Console.WriteLine("=============================================================");
+        int tarifa = 10;
+        Console.WriteLine("Ingrese el número de horas que estuvo parqueado:");
+        int horas = int.Parse(Console.ReadLine()); //Entrada
+        int monto = horas * tarifa;
+        Console.WriteLine("Usted debe pagar: Q" + monto); //Salida
+        Console.WriteLine("Ingrese la cantidad de dinero con la que pagará:");
+        int pago = int.Parse(Console.ReadLine()); //Entrada
+        if (pago < monto)
+        {
+            Console.WriteLine("Error. Pago insuficiente"); //Salida
+        }
+        else if (pago == monto)
+        {
+            Console.WriteLine("Pago exacto, no se requiere cambio. ¡Que tenga un buen día!"); //Salida
+        }
+        else
+        {
+            Console.WriteLine("Pago excedente. Su cambio es: Q" + (pago - monto)); //Salida
+            int billetes100 = (pago - monto) / 100;
+            int billetes50 = ((pago - monto) % 100) / 50;
+            int billetes20 = (((pago - monto) % 100) % 50) / 20;
+            int billetes10 = ((((pago - monto) % 100) % 50) % 20) / 10;
+            int billetes5 = (((((pago - monto) % 100) % 50) % 20) % 10) / 5;
+            int billetes1 = (((((pago - monto) % 100) % 50) % 20) % 10) % 5;
+            Console.WriteLine("Su cambio se compone de: ");
+            Console.WriteLine(billetes100 + " billetes de Q100");
+            Console.WriteLine(billetes50 + " billetes de Q50");
+            Console.WriteLine(billetes20 + " billetes de Q20");
+            Console.WriteLine(billetes10 + " billetes de Q10");
+            Console.WriteLine(billetes5 + " billetes de Q5");
+            Console.WriteLine(billetes1 + " billetes de Q1");
+        } 
+    }
+}
---
+++ L5_JAML_1101226/README.md
@@ -0,0 +1,66 @@
+# Análisis de Entradas, Salidas y Procesos
+
+
+
+#### Desafío #1
+
+
+
+Entradas: La entrada es el número que el programa solicita al usuario.
+
+
+
+Proceso: El programa evalúa si el número es menor, mayor o igual a cero.
+
+
+
+Salidas: El programa muestra un mensaje al usuario como respuesta al dato ingresado, luego de evaluar las condiciones, indica si es positivo, negativo o neutro.
+
+
+
+#### Desafío #2
+
+
+
+Entradas: El usuario le indica al programa un año.
+
+
+
+Proceso: El programa evalúa si el año es divisible entre 4 y es divisible entre 100 o es divisible entre 400.
+
+
+
+Salidas: El programa indica si el año es bisiesto o no después de realizar el proceso.
+
+
+
+#### Desafío #3
+
+
+
+Entradas: El usuario le indica al programa su salario mensual, asimismo, le indica si tiene o no una multa en su pago.
+
+
+
+Proceso: El programa primero evalúa el salario indicado por el usuario, luego evalúa si el usuario tiene multa o no. En base a estos datos, realiza el proceso.
+
+
+
+Salidas: Luego de realizar el proceso, el programa le indica al usuario la cantidad que debe pagar según su salario y si tiene multa o no.
+
+
+
+#### Desafío #4
+
+
+
+Entradas: El usuario indica la cantidad de horas parqueado y la cantidad de dinero con la que pagará.
+
+
+
+Proceso: El programa evalúa tres condiciones: Si el pago es menor al monto. Si el pago es igual al monto. Si el pago es mayor al monto, en este último, el programa realiza otro procedimiento de cálculo.
+
+
+
+Salidas: El programa inicia mostrando al usuario la cantidad que debe pagar, si paga menos al monto indicado; el programa dará error, si paga lo mismo que el monto indicado; el programa indicará que no se requiere cambio, si paga más que el monto indicado; el programa mostrará la cantidad de su cambio y la cantidad de billetes.
+
